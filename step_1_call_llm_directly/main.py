8b965d829c21037a6518323ea6c19805c05abef5
Ali Soltani <ali.soltani@red-gate.com>
2026-01-18 10:47:09 +0000

    Add LlmClient class

--- step_1_call_llm_directly/main.py
+++ step_1_call_llm_directly/main.py
@@ -5,8 +5,17 @@ from langchain_anthropic import ChatAnthropic
 
 load_dotenv()
 
-llm = ChatAnthropic(model="claude-3-haiku-20240307", temperature=0)
-user_prompt = input("Enter your prompt: ")
-response = llm.invoke(user_prompt)
-print(response.content)
+class LlmClient():
+    """A simple wrapper around the ChatAnthropic LLM client"""
+    def __init__(self, model: str = "claude-3-haiku-20240307", temperature: int = 0):
+        self.llm = ChatAnthropic(model=model, temperature=temperature)
 
+    def invoke(self, prompt: str):
+        return self.llm.invoke(prompt) 
+
+
+if __name__ == "__main__":
+    user_prompt = input("Enter your prompt: ")
+    llm = LlmClient()
+    response = llm.invoke(user_prompt)
+    print(response.content)
